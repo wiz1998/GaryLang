cfa87a4f0063def3e620b2c8c24448111b4be88c
Jordan Lee 'Gary Gary' Kelwick-Richards <jordangarygarykelwickrichards@gmail.com>
2023-07-22 15:46:08 +0100

    Init

---
+++ HelloWorld.asm
@@ -0,0 +1,17 @@
+section .text
+global _start
+
+_start:
+    mov rdi, 0x1
+    mov rsi, hello
+    mov rdx, helloLen
+    mov rax, 0x1
+    syscall
+
+    xor rdi, rdi
+    mov rax, 0x3c
+    syscall
+
+section .data
+    hello db "Hello World", 0xa
+    helloLen equ $-hello
